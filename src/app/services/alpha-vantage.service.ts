751306cd77fd6374e07547a5222da455654626c8
Keith Laptop <Keithblack4290@gmail.com>
2025-04-15 14:34:15 -0600

    SSR

--- src/app/services/alpha-vantage.service.ts
+++ src/app/services/alpha-vantage.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 import { environment } from '../../environments/environment/dev';
 import { WatchlistItem } from '../models/watchlist-item.model';
 
@@ -11,11 +11,20 @@ import { WatchlistItem } from '../models/watchlist-item.model';
 export class AlphaVantageService {
   private apiUrl = environment.apiUrl;
   private dashboardEndpoint = '/api/Dashboard';
+  private isBrowser: boolean;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   // Fetch stock data through the backend
   getStockData(symbol: string): Observable<any> {
+    if (!this.isBrowser) {
+      return of({});
+    }
     return this.http.get<any>(
       `${this.apiUrl}${this.dashboardEndpoint}/fetch/${symbol}`
     );
@@ -43,6 +52,9 @@ export class AlphaVantageService {
 
   // Get watchlist items
   getWatchlist(): Observable<any[]> {
+    if (!this.isBrowser) {
+      return of([]); // Return empty array during SSR
+    }
     return this.http.get<any[]>(
       `${this.apiUrl}${this.dashboardEndpoint}/watchlist`
     );
